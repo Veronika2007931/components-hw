6096a9a5d2e43d0d1abdc97c07541eb41a8dc730
Veronika2007931 <113263850+Veronika2007931@users.noreply.github.com>
2024-01-04 21:20:11 +0200

    додає

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-// import {Greeting} from "./components/Greeting.jsx"
+import {Greeting} from "./components/Greeting.jsx"
 // import {Message}  from "./components/Message.jsx"
-import {Button}  from "./components/Button.jsx"
+
 import './App.css';
 
 function App() {
   return (
-    // < Greeting  name = "Ірина"/> ,
-    // <Message text = "Лише 0,63% людей натискають на другу сторінку результатів пошуку Google. Якщо користувачі не зможуть знайти те, що шукають на першій сторінці, вони швидше змінять свій запит ніж, перейдуть на другу сторінку видачі."/>,
-    < Button onClick = {console.log("clicked button")}/>
-  );
+    < Greeting  name = "Ірина"/> 
+    // <Message text = "Лише 0,63% людей натискають на другу сторінку результатів пошуку Google. Якщо користувачі не зможуть знайти те, що шукають на першій сторінці, вони швидше змінять свій запит ніж, перейдуть на другу сторінку видачі."/>
+    );
 }
 
 export default App;
